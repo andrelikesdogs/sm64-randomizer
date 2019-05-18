495d963b9aa19155e28722547ebbf442bf71b9d5
Andre Meyer <andre.meyer93@gmail.com>
2019-05-18 17:38:36 +0200

    finish web stuff for now, still missing submit logic. merged config file for web and cli. missing gui

--- scripts/build_mac_osx.sh
+++ scripts/build_mac_osx.sh
@@ -1,23 +1,32 @@
 rm -rf dist/main
-bin/pyinstaller --noconfirm --onefile --windowed --noconsole --osx-bundle-identifier "com.andremeyer.sm64-randomizer" --add-data="README.md:." --add-data="LICENSE:." --i "favicon.icns" main.py
+bin/pyinstaller \
+--noconfirm \
+--onefile \
+--windowed \
+--noconsole \
+--osx-bundle-identifier "com.andremeyer.sm64-randomizer" \
+--add-data="README.md:." \
+--add-data="LICENSE:." \
+--add-data="Data:." \
+--i "favicon.icns" \
+main.py
+
+mv dist/main.app dist/SM64\ Randomizer.app
+mv dist/main dist/SM64\ Randomizer\ CLI
 
 # prepare release folder
-rm -rf ./release
-mkdir ./release
-
-mv dist/main.app release/SM64\ Randomizer.app
-mv dist/main release/SM64\ Randomizer\ CLI
+mkdir -p ./release
 
 # pack as zip
 VERSION="$(python -c 'from __version__ import __version__; print(__version__)')"
-rm dist/sm64-randomizer-${VERSION}-mac-osx.zip
-cp ./README.md ./release
-cp ./LICENSE ./release
-mkdir ./release/3rdparty
-cp -v ./3rdparty/*mac* ./release/3rdparty
-cp -v ./3rdparty/LICENSE* ./release/3rdparty
-cp -v ./3rdparty/README* ./release/3rdparty
-cd ./release
-zip -r ../dist/sm64-randomizer-${VERSION}-mac-osx.zip SM64* README.md LICENSE
+rm release/sm64-randomizer-${VERSION}-mac-osx.zip
+cp ./README.md ./dist
+cp ./LICENSE ./dist
+mkdir ./dist/3rdparty
+cp -v ./3rdparty/*mac* ./dist/3rdparty
+cp -v ./Data ./Data
+cp -v ./3rdparty/LICENSE* ./dist/3rdparty
+cp -v ./3rdparty/README* ./dist/3rdparty
+cd ./dist
+zip -r ../release/sm64-randomizer-${VERSION}-mac-osx.zip SM64* README.md LICENSE 3rdparty
 cd -;
-rm -rf ./release
--- scripts/build_win.ps1
+++ scripts/build_win.ps1
@@ -3,7 +3,7 @@
 # Install Python 3.7.x (newest minor) both 32 and 64 bit. If you only require one and not the other, feel free to remove/comment out the other.
 # Make sure Python is installed in the default folder in your Users Directory
 
-# 32 bit
+# setup (read version file, create folders)
 $env:Path = ";$home\AppData\Local\Programs\Python\Python37-32"
 $env:Path += ";$home\AppData\Local\Programs\Python\Python37-32\Scripts"
 
@@ -11,16 +11,29 @@ $RandoVersion = python -c 'from __version__ import __version__; print(__version_
 
 mkdir ./release -ErrorAction SilentlyContinue
 
+$env:Path = ";$home\AppData\Local\Programs\Python\Python37-32"
+$env:Path += ";$home\AppData\Local\Programs\Python\Python37-32\Scripts"
+# 32 bit
 pip install -r .\requirements.txt
 Remove-Item ./dist/ -Recurse
 Remove-Item ./release/*.exe
-pyinstaller --noconfirm --onefile --add-data="README.md;." --add-data="LICENSE;." --add-data="favicon.ico;." -i "favicon.ico" main.py
-mv "dist/main.exe" "release/SM64 Randomizer CLI.exe"
-pyinstaller --noconfirm --noconsole --onefile --add-data="README.md;." --add-data="LICENSE;." --add-data="favicon.ico;." --add-data="3rdparty/LICENSE;3rdparty/." --add-data="3rdparty/README.md;3rdparty/." -i "favicon.ico" main.py
-mv "dist/main.exe" "release/SM64 Randomizer GUI.exe"
-$ArchiveName = "release/sm64_randomizer-$RandoVersion-win32.zip"
+pyinstaller --noconfirm --onefile --add-data="README.md;." --add-data="LICENSE;." --add-data="favicon.ico;." --add-data="Data;." -i "favicon.ico" main.py
+mv "dist/main.exe" "dist/SM64 Randomizer CLI.exe"
+pyinstaller --noconfirm --noconsole --onefile --add-data="README.md;." --add-data="LICENSE;." --add-data="favicon.ico;." --add-data="Data;." --add-data="3rdparty/LICENSE;3rdparty/." --add-data="3rdparty/README.md;3rdparty/." -i "favicon.ico" main.py
+mv "dist/main.exe" "dist/SM64 Randomizer GUI.exe"
+
+mkdir ./dist/3rdparty -ErrorAction SilentlyContinue
+
+cp 3rdparty/*win* ./dist/3rdparty
+cp 3rdparty/LICENSE ./dist/3rdparty
+cp 3rdparty/README.md ./dist/3rdparty
+cp Data ./dist
+
+$ArchiveName = "release/sm64-randomizer-$RandoVersion-win32.zip"
 del $ArchiveName -ErrorAction SilentlyContinue
-& 'C:\Program Files\7-Zip\7z.exe' -tzip a $ArchiveName release/*.*
+cd dist
+& 'C:\Program Files\7-Zip\7z.exe' -tzip a ../$ArchiveName ./*.exe 3rdparty/* *.md LICENSE
+cd ..
 
 # 64 bit
 $env:Path = ";$home\AppData\Local\Programs\Python\Python37"
@@ -29,10 +42,20 @@ $env:Path += ";$home\AppData\Local\Programs\Python\Python37\Scripts"
 pip install -r .\requirements.txt
 Remove-Item ./dist/ -Recurse
 Remove-Item ./release/*.exe
-pyinstaller --noconfirm --onefile --add-data="README.md;." --add-data="LICENSE;." --add-data="favicon.ico;." -i "favicon.ico" main.py
-mv "dist/main.exe" "release/SM64 Randomizer CLI.exe"
-pyinstaller --noconfirm --noconsole --onefile --add-data="README.md;." --add-data="LICENSE;." --add-data="favicon.ico;." --add-data="3rdparty/LICENSE;3rdparty/." --add-data="3rdparty/README.md;3rdparty/." -i "favicon.ico" main.py
-mv "dist/main.exe" "release/SM64 Randomizer GUI.exe"
-$ArchiveName = "release/sm64_randomizer-$RandoVersion-win32.zip"
+pyinstaller --noconfirm --onefile --add-data="README.md;." --add-data="LICENSE;." --add-data="favicon.ico;." --add-data="Data;." -i "favicon.ico" main.py
+mv "dist/main.exe" "dist/SM64 Randomizer CLI.exe"
+pyinstaller --noconfirm --noconsole --onefile --add-data="README.md;." --add-data="LICENSE;." --add-data="favicon.ico;." --add-data="Data;." --add-data="3rdparty/LICENSE;3rdparty/." --add-data="3rdparty/README.md;3rdparty/." -i "favicon.ico" main.py
+mv "dist/main.exe" "dist/SM64 Randomizer GUI.exe"
+
+mkdir ./dist/3rdparty -ErrorAction SilentlyContinue
+
+cp 3rdparty/*win* ./dist/3rdparty
+cp 3rdparty/LICENSE ./dist/3rdparty
+cp 3rdparty/README.md ./dist/3rdparty
+cp Data ./dist
+
+$ArchiveName = "release/sm64-randomizer-$RandoVersion-win64.zip"
 del $ArchiveName -ErrorAction SilentlyContinue
-& 'C:\Program Files\7-Zip\7z.exe' -tzip a $ArchiveName release/*.exe release/3rdparty/* release/*.md release/LICENSE
+cd dist
+& 'C:\Program Files\7-Zip\7z.exe' -tzip a ../$ArchiveName ./*.exe 3rdparty/* *.md LICENSE
+cd ..
