3771b8801765a9d48b6d2ddc28ff9c7478a27697
Andre Meyer <andre.meyer93@gmail.com>
2019-06-06 18:34:25 +0200

    fix broken mem address

--- Enhancements/GameplayEnhancements.py
+++ Enhancements/GameplayEnhancements.py
@@ -8,7 +8,7 @@ class Gameplay:
   def disable_all_cutscenes(self):
     self.tweaking.add_asm_patch(
       [
-        (0x6BF0, bytes([0x24, 0x00])), # Peach Cutscene
+        (0x6BD4, bytes([0x24, 0x00])), # Peach Cutscene
         (0x6D90, bytes([0x24, 0x10, 0x00, 0x00])), # Lakitu Cutscene
         (0x4B7C, bytes([0x24, 0x00])), # Level Intros #1
         (0x4924, bytes([0x10, 0x00])), # Level Intros #2
