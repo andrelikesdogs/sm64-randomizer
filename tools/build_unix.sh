14fd0daec92fec0c081f876215c320104c41fcc7
Andre Meyer <andre.meyer@adesso.de>
2021-04-04 16:38:34 +0200

    fix rm failing when file not exists

--- tools/build_unix.sh
+++ tools/build_unix.sh
@@ -9,7 +9,7 @@ mkdir -p ./release
 
 # pack as zip
 VERSION="$(python -c 'from __version__ import __version__; print(__version__)')"
-rm release/sm64-randomizer-${VERSION}-mac-osx.zip
+rm -f ../release/sm64-randomizer-${VERSION}-mac-osx.zip
 cp ./README.md ./dist
 cp ./LICENSE ./dist
 mkdir -p ./dist/3rdparty
