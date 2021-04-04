d80d82741e15be555d0abfcd9cab01022750780f
Andre Meyer <andre.meyer@adesso.de>
2021-04-04 21:01:21 +0200

    change artifact workflow

--- tools/build_unix.sh
+++ tools/build_unix.sh
@@ -1,15 +1,12 @@
-rm -rf dist/main
+rm -rf dist/
+mkdir -p dist
 set -e # exit on error
 pyinstaller main-osx.spec
 
-mv dist/main dist/SM64\ Randomizer\ Generator
-
-# prepare release folder
-mkdir -p ./release
+#mv dist/main dist/SM64\ Randomizer\ Generator
 
 # pack as zip
 VERSION="$(python -c 'from __version__ import __version__; print(__version__)')"
-rm -f ../release/sm64-randomizer-${VERSION}-unix.zip
 cp ./README.md ./dist
 cp ./LICENSE ./dist
 mkdir -p ./dist/3rdparty
@@ -20,10 +17,7 @@ cp -vr ./Config ./dist
 cp -vr ./Assets ./dist
 cp -v ./3rdparty/LICENSE* ./dist/3rdparty
 cp -v ./3rdparty/README* ./dist/3rdparty
-cd ./dist
-zip -r ../release/sm64-randomizer-${VERSION}-unix.zip SM64* README.md LICENSE 3rdparty Data Config Assets
-cd -;
 set +e
 
-# save filename in env variables for later build-step
-echo "RELEASE_FILENAME=sm64-randomizer-${VERSION}-unix.zip" >> $GITHUB_ENV
+# save version in env variables for later build-step
+echo "VERSION=${VERSION}" >> $GITHUB_ENV
--- tools/build_win.ps1
+++ tools/build_win.ps1
@@ -1,17 +1,9 @@
-# Some notes to make this work for you:
-# Install 7z (if you don't install it in the default folder or not on C:/, change the last line)
-# Install Python 3.7.x (newest minor) both 32 and 64 bit. If you only require one and not the other, feel free to remove/comment out the other.
-# Make sure Python is installed in the default folder in your Users Directory
-
 $RandoVersion = python -c 'from __version__ import __version__; print(__version__)'
 echo "Randomizer Version: $RandoVersion"
-
-mkdir ./release -ErrorAction SilentlyContinue
 
 # 32 bit
 pip install -r .\requirements.txt
 Remove-Item ./dist/ -Recurse -ErrorAction SilentlyContinue
-Remove-Item ./release/*.exe
 pyinstaller main-win.spec
 mv "dist/main.exe" "dist/SM64 Randomizer Generator.exe"
 
@@ -24,8 +16,5 @@ Copy-Item ./Config -Destination ./dist -Recurse -Force
 Copy-Item ./Data -Destination ./dist -Recurse -Force
 Copy-Item ./Assets -Destination ./dist -Recurse -Force
 
-$ArchiveName = "release/sm64-randomizer-$RandoVersion-win32.zip"
-del $ArchiveName -ErrorAction SilentlyContinue
-cd dist
-& 'C:\Program Files\7-Zip\7z.exe' -tzip a ../$ArchiveName ./*.exe 3rdparty/* *.md LICENSE ./Data ./Config ./Assets
-cd ..
+# output version as env var for other build steps
+Write-Output "::echo VERSION=$RandoVersion"
