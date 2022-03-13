209e143584ff19ed8f96630dafce327a28bcb3eb
Andre <andre.meyer93+alt@gmail.com>
2022-03-13 21:46:41 +0100

    update website

--- docs/configurableParams.js
+++ docs/configurableParams.js
@@ -1,203 +1,134 @@
-configurableParams = [
+[
   {
-    "name": "seed",
-    "label": "Random Seed",
-    "category": "gameplay",
-    "type": "text",
-    "help": "Allows you to play the same version as a friend, simply enter the same seed as them and you will be playing the exact same ROM."
+    name: "seed",
+    label: "Random Seed",
+    category: "gameplay",
+    type: "text",
+    help: "Allows you to play the same version as a friend, simply enter the same seed as them and you will be playing the exact same ROM.",
   },
   {
-    "name": "shuffle-paintings",
-    "label": "Painting (Art) randomization",
-    "category": "aesthetics",
-    "type": "select",
-    "default": "match",
-    "options": [
+    name: "shuffle-paintings",
+    label: "Painting (Art) randomization",
+    category: "aesthetics",
+    type: "select",
+    default: "match",
+    options: [
+      { value: "vanilla", label: "Off" },
+      { value: "match", label: "Match Levels" },
       {
-        "value": "vanilla",
-        "label": "Off"
+        value: "replace-unknown",
+        label: "Use Custom Paintings for Unknown Paintings",
       },
-      {
-        "value": "match",
-        "label": "Match Levels"
-      },
-      {
-        "value": "replace-unknown",
-        "label": "Use Custom Paintings for Unknown Paintings"
-      },
-      {
-        "value": "random",
-        "label": "Randomized"
-      }
+      { value: "random", label: "Randomized" },
     ],
-    "help": "Change the behaviour of how the paintings in the castle are shuffled (\"match\" - matches randomized levels, i.e. painting = level, \"random\" - independently randomize paintings, \"off\" - leave paintings untouched)"
+    help: 'Change the behaviour of how the paintings in the castle are shuffled ("match" - matches randomized levels, i.e. painting = level, "random" - independently randomize paintings, "off" - leave paintings untouched)',
   },
   {
-    "name": "custom-painting-author",
-    "label": "Custom Painting (Art) Author",
-    "category": "aesthetics",
-    "type": "select",
-    "default": "mika",
-    "options": [
-      {
-        "value": "disable",
-        "label": "Disabled"
-      },
-      {
-        "value": "mika",
-        "label": "Mika"
-      }
+    name: "custom-painting-author",
+    label: "Custom Painting (Art) Author",
+    category: "aesthetics",
+    type: "select",
+    default: "mika",
+    options: [
+      { value: "disable", label: "Disabled" },
+      { value: "mika", label: "Mika" },
     ],
-    "help": "This property allows changing the custom paintings to a different author, if you want to add your own, see the sm64.vanilla.yml"
+    help: "This property allows changing the custom paintings to a different author, if you want to add your own, see the sm64.vanilla.yml",
   },
   {
-    "name": "shuffle-skybox",
-    "label": "Sky randomization",
-    "category": "aesthetics",
-    "type": "checkbox",
-    "default": false,
-    "help": "Randomizes the sky-texture between different levels. The black skybox is excluded."
+    name: "shuffle-skybox",
+    label: "Sky randomization",
+    category: "aesthetics",
+    type: "checkbox",
+    default: false,
+    help: "Randomizes the sky-texture between different levels. The black skybox is excluded.",
   },
   {
-    "name": "shuffle-entries",
-    "label": "Entrance Randomizer",
-    "category": "gameplay",
-    "type": "checkbox",
-    "default": {
-      "CLI": false,
-      "WEB": true
+    name: "shuffle-entries",
+    label: "Entrance Randomizer",
+    category: "gameplay",
+    type: "checkbox",
+    default: {
+      CLI: false,
+      WEB: true,
     },
-    "help": "Shuffles the levelentries. When you enter a level, you will end up at a random one."
+    help: "Shuffles the levelentries. When you enter a level, you will end up at a random one.",
   },
   {
-    "name": "shuffle-mario-outfit",
-    "label": "Mario's Outfit Randomizer",
-    "category": "aesthetics",
-    "type": "checkbox",
-    "default": {
-      "CLI": false,
-      "WEB": true
+    name: "shuffle-mario-outfit",
+    label: "Mario's Outfit Randomizer",
+    category: "aesthetics",
+    type: "checkbox",
+    default: {
+      CLI: false,
+      WEB: true,
     },
-    "help": "Randomizes parts of Marios Outfit."
+    help: "Randomizes parts of Marios Outfit.",
   },
   {
-    "name": "shuffle-music",
-    "label": "Music Randomizer",
-    "category": "aesthetics",
-    "type": "checkbox",
-    "default": {
-      "CLI": false,
-      "WEB": false
+    name: "shuffle-music",
+    label: "Music Randomizer",
+    category: "aesthetics",
+    type: "checkbox",
+    default: {
+      CLI: false,
+      WEB: false,
     },
-    "help": "Randomizes most songs in the game."
+    help: "Randomizes most songs in the game.",
   },
   {
-    "name": "shuffle-objects",
-    "label": "Object Shuffle",
-    "category": "gameplay",
-    "type": "checkbox",
-    "default": {
-      "CLI": false,
-      "WEB": true
+    name: "shuffle-objects",
+    label: "Object Shuffle",
+    category: "gameplay",
+    type: "checkbox",
+    default: {
+      CLI: false,
+      WEB: true,
     },
-    "help": "Shuffles Objects in Levels"
+    help: "Shuffles Objects in Levels",
   },
   {
-    "name": "shuffle-colors",
-    "label": "Randomize Colors",
-    "category": "aesthetics",
-    "type": "checkbox",
-    "default": {
-      "CLI": false,
-      "WEB": true
+    name: "shuffle-colors",
+    label: "Randomize Colors",
+    category: "aesthetics",
+    type: "checkbox",
+    default: {
+      CLI: false,
+      WEB: true,
     },
-    "help": "Shuffle various colors in the game"
+    help: "Shuffle various colors in the game",
   },
   {
-    "name": "shuffle-text",
-    "label": "Randomize Dialog/Text",
-    "category": "aesthetics",
-    "type": "checkbox",
-    "default": {
-      "CLI": false,
-      "WEB": true
+    name: "shuffle-text",
+    label: "Randomize Dialog/Text",
+    category: "aesthetics",
+    type: "checkbox",
+    default: {
+      CLI: false,
+      WEB: true,
     },
-    "help": "Shuffle Dialog text, for signs, npc dialog, level dialog and prompts."
+    help: "Shuffle Dialog text, for signs, npc dialog, level dialog and prompts.",
   },
   {
-    "name": "disable-cutscenes",
-    "label": "Disable Cutscenes/Intro",
-    "category": "gameplay",
-    "type": "checkbox",
-    "default": {
-      "CLI": false,
-      "WEB": true
+    name: "shuffle-instruments",
+    label: "Instrument-Set Shuffle",
+    category: "aesthetics",
+    type: "checkbox",
+    default: {
+      CLI: false,
+      WEB: false,
     },
-    "help": "Disables some of the games cutscenes. (Peach Intro, Lakitu Intro, Bowser-Text on Entry)"
+    help: "Shuffles instrument sounds around. Many different objects and songs use different instrument sets, as the N64 can't load all at once. This will shuffle them around. Might be wacky.",
   },
   {
-    "name": "disable-starwarp",
-    "label": "Disable Star-Warp",
-    "category": "gameplay",
-    "type": "checkbox",
-    "disabled": "work-in-progress",
-    "default": {
-      "CLI": false,
-      "WEB": false
+    name: "disable-cutscenes",
+    label: "Disable Cutscenes/Intro",
+    category: "gameplay",
+    type: "checkbox",
+    default: {
+      CLI: false,
+      WEB: true,
     },
-    "help": "Disables automatically leaving the level when you collect a star. This way, all stars act like a 100-Coin star or a Bowser 8-Reds Star"
+    help: "Disables some of the games cutscenes. (Peach Intro, Lakitu Intro, Bowser-Text on Entry)",
   },
-  {
-    "name": "stardoor-requirements",
-    "label": "Stardoor Requirements",
-    "category": "gameplay",
-    "disabled": "work-in-progress",
-    "type": "select",
-    "options": [
-      {
-        "value": "vanilla",
-        "label": "Vanilla"
-      },
-      {
-        "value": "random",
-        "label": "Random"
-      },
-      {
-        "value": "open",
-        "label": "All Open"
-      }
-    ],
-    "default": "vanilla",
-    "help": "Changes how the doors to levels require different amounts of stars to be collected beforehand. Random means all doors require stars, but the amount will be random. Open simply means all doors are open from the start."
-  },
-  {
-    "name": "keydoor-requirements",
-    "label": "Keydoor Requirements",
-    "category": "gameplay",
-    "disabled": "work-in-progress",
-    "type": "select",
-    "options": [
-      {
-        "value": "vanilla",
-        "label": "Vanilla"
-      },
-      {
-        "value": "open",
-        "label": "All Open"
-      }
-    ],
-    "default": "vanilla",
-    "help": "Changes how the locked keydoors in the castle work. Vanilla means you will require the usual keys from the Bowser fights. Open means they're simply open from the start."
-  },
-  {
-    "name": "shuffle-instruments",
-    "label": "Instrument-Set Shuffle",
-    "category": "aesthetics",
-    "type": "checkbox",
-    "default": {
-      "CLI": false,
-      "WEB": false
-    },
-    "help": "Shuffles instrument sounds around. Many different objects and songs use different instrument sets, as the N64 can't load all at once. This will shuffle them around. Might be wacky."
-  }
-]
+];
